03600b5bbeaacf0d9df95d66f10130d6f1be81d9
DINBRASIL-MKT <thiago@dinbrasil.com.br>
2025-10-14 14:26:41 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+function calcular() {
+    let L = parseFloat(document.getElementById('L').value);
+    let TA = parseFloat(document.getElementById('TA').value) / 100;
+    let TM = parseFloat(document.getElementById('TM').value);
+    let TC = parseFloat(document.getElementById('TC').value) / 100;
+
+    if (isNaN(L) || isNaN(TA) || isNaN(TM) || isNaN(TC)) {
+      document.getElementById('resultado').innerText = "Preencha todos os campos.";
+      return;
+    }
+
+    let perdaMensal = (L * TA) * TM * TC;
+    let perdaAnual = perdaMensal * 12;
+
+    document.getElementById('resultado').innerHTML = `
+       Perda estimada: <br>
+      <strong>R$ ${perdaMensal.toLocaleString('pt-BR', {minimumFractionDigits: 2})}</strong> / mês <br>
+      <small>R$ ${perdaAnual.toLocaleString('pt-BR', {minimumFractionDigits: 2})} / ano</small>
+    `;
+  }
